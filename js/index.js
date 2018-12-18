5410338b3dc59fb2203d8117868d15b0d661e878
LZF760711343 <18819352078@163.com>
2018-12-18 11:01:58 +0800

    添加一条三国游戏web链接

---
+++ js/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * Created by 小Z on 2017/10/6.
+ */
+var share = (function () {
+    function share() {
+        this.Data = [];
+        var _this = this;
+        _this.Data = [{ A: "11" }];
+    }
+    return share;
+}());
+exports.share = share;
+//# sourceMappingURL=1.js.map
